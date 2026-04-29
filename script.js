04177a48ee2cf85586a0160c6b9b520413a32f9c
Yusuke Ho <yhowtcc@my.waketech.edu>
2026-04-29 14:05:42 -0400

    29

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
+
+
+//⅃  Color: Dark Blue
+//L  Color: Light orange
+//■  Color: Yellow
+//|  Color: Light Blue
+//S  Color: Green
+//Z  Color: Red
+//T  Color: Purple
+
+x=10
+y=20
+
 let tbl = [];
 let grid = document.createElement(`table`);
 tbl = []; // Reset the global tbl array
 
-for (r = 0; r < 40; r++) {
-    let tableRow = document.createElement(`tr`);
-    let rowArray = []; // Make a row array to hold the Cells
-            
-    for (c = 0; c < 10; c++) {
-        let tableColumn = document.createElement(`td`); // c is x and r is y
-        tableColumn.innerText = r
-        rowArray.push(tableColumn); // Push the actual Cell object
-        tableRow.appendChild(tableColumn)
-    }
-    tbl.push(rowArray); // Push the completed row into tbl
-    grid.appendChild(tableRow);
-}
-document.body.appendChild(grid);
 
+
+
+function createBoard() {
+    let grid = document.createElement(`table`);
+    grid.style.borderCollapse = "collapse";
+    board = []; 
+
+    for (let r = 0; r < y; r++) {
+        let tableRow = document.createElement(`tr`);
+        let rowArray = []; 
+        
+        for (let c = 0; c < x; c++) {
+            let tableColumn = new Cell(c, r);
+            rowArray.push(tableColumn); 
+            tableRow.appendChild(tableColumn.table);
+        }
+        board.push(rowArray); 
+        grid.appendChild(tableRow);
+    }
+    document.body.appendChild(grid);
+}
+
+
+class Cell {
+    x;
+    y;
+    table;
+    value = 0;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.table = this.generateCellDiv(); 
+        this.value = Cell.value;
+    } 
+    
+    generateCellDiv() {
+        let td = document.createElement(`td`);
+        
+        // Slightly smaller cells so a height of 20 fits on screen
+        td.style.width = "15px";
+        td.style.height = "15px"; 
+        td.style.backgroundColor = "transparent";
+        
+        return td;
+    }
+}
+
+
+
+
+createBoard()
