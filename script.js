5c0de99097b5d3b1ee43d2ad79079b6ef2c825c6
Yusuke Ho <yhowtcc@my.waketech.edu>
2026-05-04 14:07:59 -0400

    mon

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+const column = 10;
+const row = 20;
+let board = [];
+let currentShape;
+let currentColor;
 
 
 
 
-x=10
-y=20
+// Shapes
+let pieces = [
+    [[1,1,1,1]], // | piece
+    [[1,1],[1,1]], // ■ piece
+    [[0,1,0],[1,1,1]], // T piece
+    [[1,0,0],[1,1,1]], // L piece
+    [[0,0,1],[1,1,1]], // ⅃ piece
+    [[0,1,1],[1,1,0]], // S piece
+    [[1,1,0],[0,1,1]]  // Z piece
+];
 
+// Colors
+const colors = ["cyan", "yellow", "purple", "orange", "blue", "green", "red"];
 
 let grid = document.createElement(`table`);
 
@@ -14,58 +29,31 @@ let grid = document.createElement(`table`);
 function createBoard() {
     let grid = document.createElement(`table`);
     grid.style.borderCollapse = "collapse";
-    board = []; 
+    grid.style.backgroundColor = "black"
+     
 
-    for (let r = 0; r < y; r++) {
+    for (let r = 0; r < row; r++) {
         let tableRow = document.createElement(`tr`);
         let rowArray = []; 
         
-        for (let c = 0; c < x; c++) {
-            let tableColumn = new Cell(c, r);
-            rowArray.push(tableColumn); 
-            tableRow.appendChild(tableColumn.table);
+        for (let c = 0; c < column; c++) {
+            let cell = new Cell(c, r);
+            rowArray.push(cell); 
+            tableRow.appendChild(cell.td);
         }
         board.push(rowArray); 
         grid.appendChild(tableRow);
     }
     document.body.appendChild(grid);
-
-    // Shapes
-    let pieces = [
-        [[1,1,1,1]], // | piece
-        [[1,1],[1,1]], // ■ piece
-        [[0,1,0],[1,1,1]], // T piece
-        [[1,0,0],[1,1,1]], // L piece
-        [[0,0,1],[1,1,1]], // ⅃ piece
-        [[0,1,1],[1,1,0]], // S piece
-        [[1,1,0],[0,1,1]]  // Z piece
-    ];
-
-    // Colors
-    let colors = ["cyan", "yellow", "purple", "orange", "blue", "green", "red"];
-
-
-    function spawnPiece() {
-        let randomIndex = Math.floor(Math.random() * pieces.length);
-        currentShape = pieces[randomIndex];
-        currentColor = colors[randomIndex];
-        currentX = 3;
-        currentY = 0;
-
-    }
 }
 
 
-class Cell {
-    x;
-    y;
-    table;
-    value = 0;
 
+class Cell {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.table = this.generateCellDiv(); 
+        this.td = this.generateCellDiv(); 
     } 
     
     generateCellDiv() {
@@ -77,8 +65,10 @@ class Cell {
         td.style.height = "15px"; 
         td.style.backgroundColor = "transparent";
 
-        td.innerText = this.value
         return td;
+    }
+    setColor(color) {
+        this.td.style.backgroundColor = color;
     }
 }
 
@@ -86,29 +76,79 @@ class Cell {
 
 
 
+class player {
+    constructor () {
+        this.spawnPiece();
+        document.addEventListener('keydown', function(key) {
+            if (key == "ArrowUp") {
+                console.log(key);
+                return;
+            }
+            if (key == "ArrowUp") {
+                console.log(key);
+                return;
+            }
+            if (key == "ArrowUp") {
+                console.log(key);
+                return;
+            }
+            if (key == "ArrowUp") {
+                console.log(key);
+                return;
+            }
 
-
-class player extends Cell {
-    currentShape;
-    currentColor;
-    currentX = 0;
-    currentY = 0;
-    gameInterval;
-
-    constructor (currentX, currentY) {
-        currentX = this.currentX;
-        currentY = this.currentY;
-        super(value);
+        })
     }
-
-    grabity () {
+    
+    spawnPiece() {
         let randomIndex = Math.floor(Math.random() * pieces.length);
         currentShape = pieces[randomIndex];
         currentColor = colors[randomIndex];
+        this.x = 3;
+        this.y = 0;
     }
 
+    gravity(){
+        this.y++
+    }
+
+}
+
+function update() {
+    for (let r = 0; r < row; r++) {
+        for (c = 0; c < column; c++) {
+            let cellColor;
+            if (board[r][c].value == 0) {
+                cellColor = "transparent";
+
+            } else {
+                cellColor = board[r][c].value;
+            }
+        board[r][c].setColor(cellColor);
+        }
+    }
+
+    for (let r = 0; r < currentShape.length; r++) {
+        for (let c = 0; c < currentShape[r].length; c++) {
+            if (currentShape[r][c] == 1) {
+                let updateY = activePlayer.y + r;
+                let updateX = activePlayer.x + c;
+
+                if (updateY < row && updateX >= 0 && updateX < column) {
+                    board[updateY][updateX].setColor(currentColor);
+                }
+            }
+        }
+    }
 }
 
 
 
 createBoard()
+let activePlayer = new player;
+
+setInterval(function() {
+    activePlayer.gravity();
+    update();
+}, 200);
+
